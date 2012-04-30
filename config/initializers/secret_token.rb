22424d6f37fe8338c4c21f0606d9ca7d8963f158
ruby -v <cuteumaarun@gmail.com>
2012-04-30 17:13:43 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoeditor::Application.config.secret_token = 'efd69e90335ba30c3dc3f3ace3e730cef0fc405e0b2e9a23a6761c41c6c151dc601c67d7e808354d21fc19b4c9c9bdc8d66c74dd666d562e855414fb58315f04'
